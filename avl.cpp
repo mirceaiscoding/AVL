b7ff1f6fa15690b5485b567878abfcfacd5c4c18
Bina Mircea <bina.mircea2001@gmail.com>
2021-05-07 11:08:00 +0300

    work in progress: AVL Node

---
+++ avl.cpp
@@ -0,0 +1,150 @@
+#include <iostream>
+using namespace std;
+
+/**
+ * @brief Node for a AVL tree
+ * 
+ */
+class Node
+{
+
+private:
+    // Value of the node
+    int value;
+
+    // Height of the node (max lenght from this node to a leaf node)
+    int height;
+
+    // Pointer to left child node
+    Node *leftChild;
+
+    // Pointer to right child node
+    Node *rightChild;
+
+public:
+    /**
+     * @brief Construct a new Node object
+     * 
+     * @param value 
+     */
+    Node(int value)
+    {
+        this -> value = value;
+        height = 1; // the node starts as a leaf
+        leftChild = NULL;
+        rightChild = NULL;
+    }
+
+    /**
+     * @brief Copy constructor
+     * 
+     * @param node 
+     */
+    Node(const Node &node)
+    {
+        value = node.value;
+        height = node.height;
+        leftChild = node.leftChild;
+        rightChild = node.rightChild;
+    }
+
+    /**
+     * @brief Destroy the Node object
+     * 
+     */
+    ~Node()
+    {
+
+        // TODO: add destructor
+    }
+
+    /**
+     * @brief Get the value
+     * 
+     * @return value of node 
+     */
+    int getValue()
+    {
+        return value;
+    }
+
+    /**
+     * @brief Get the height
+     * 
+     * @return height of node 
+     */
+    int getHeight()
+    {
+        return height;
+    }
+
+    /**
+     * @brief Get the left child node
+     * 
+     * @return pointer to left child node
+     */
+    Node *getLeftChild()
+    {
+        return leftChild;
+    }
+
+    /**
+     * @brief Get the right child node
+     * 
+     * @return pointer to right child node
+     */
+    Node *getRightChild()
+    {
+        return rightChild;
+    }
+
+    /**
+     * @brief Set the value
+     * 
+     * @param value value to set
+     */
+    void setValue(int value)
+    {
+        this->value = value;
+    }
+
+    /**
+     * @brief Set the height
+     * 
+     * @param height height to set
+     */
+    void setHeight(int height)
+    {
+        this->height = height;
+    }
+
+    /**
+     * @brief Set the left child
+     * 
+     * @param leftChild 
+     */
+    void setLeftChild(Node leftChild)
+    {
+        this->leftChild = new Node(leftChild);
+    }
+
+    /**
+     * @brief Set the right child
+     * 
+     * @param rightChild 
+     */
+    void setRightChild(Node rightChild)
+    {
+        this->rightChild = new Node(rightChild);
+    }
+};
+
+int main()
+{
+
+    Node n1(10);
+    Node n2(12);
+    n1.setLeftChild(n2);
+
+    cout << n1.getHeight();
+}
